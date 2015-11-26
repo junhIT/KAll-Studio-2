d1f2ee01547db7c494375c1a6adcfd156c8d3585
dkswlgh000 <anjiho0328@naver.com>
2015-11-26 20:49:58 +0900

    오류 수정

--- Assets/bullet.js
+++ Assets/bullet.js
@@ -1,24 +1,24 @@
 ﻿#pragma strict
 
 private var bulletSpeed : float ;
- 
+
 function Start () {
- 
+
     bulletSpeed = 50 ;
- 
+
     gameObject.GetComponent(Rigidbody).AddForce(transform.up * bulletSpeed) ;
 }
 
 function OnTriggerEnter (enterCollider : Collider) {
- 
+
     if (enterCollider.gameObject.name == "bulletEraser"){
      Destroy (gameObject) ;
  }
- else if (enterCollider.gameObject.name == "npc_Earth") {
- 
+ else if (enterCollider.gameObject.name == "npc1") {
+
         Destroy (gameObject) ;
         Destroy (enterCollider.gameObject) ;
- 
+
     }
 
 
