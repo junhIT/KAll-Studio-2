b5c682c0b419a32addf7adb35544d4c2e4db8317
tjsej1324 <tjsej1324@naver.com>
2015-11-27 19:03:54 +0900

    Stage1 background move.

---
+++ Assets/stage1_bg.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+    if (gameObject.tag == "stage1_bg_star1") {
+ 
+        if (gameObject.transform.position.y > -0.8) {
+ 
+            gameObject.transform.position.y -= 0.003 ;
+ 
+        }
+        else 
+        {
+            gameObject.transform.position.y = 3.0 ;
+ 
+        }
+    }
+
+    if (gameObject.tag == "stage1_bg_star2") {
+ 
+        if (gameObject.transform.position.y > -0.8) {
+ 
+            gameObject.transform.position.y -= 0.001 ;
+ 
+        }
+        else 
+        {
+            gameObject.transform.position.y = 3.0 ;
+ 
+        }
+    }
+}
